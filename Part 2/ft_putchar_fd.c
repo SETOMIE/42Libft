87f186286e3406e6e8857a3ac998b04a96072297
SETOMIE <setomieart@gmail.com>
2025-11-11 12:50:49 +0800

    jeff

--- Part 2/ft_putchar_fd.c
+++ Part 2/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: asyeo <asyeo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 11:25:02 by asyeo             #+#    #+#             */
-/*   Updated: 2025/11/11 10:10:17 by asyeo            ###   ########.fr       */
+/*   Updated: 2025/11/11 12:41:28 by asyeo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,3 +29,8 @@ void	ft_putchar_fd(char c, int fd)
 	write (fd, &c, 1);
 	return (0);
 }
+
+/*
+1st para is file descriptor.
+2nd para is address of the char.
+*/
